95b0af3a198235e0d68f2b074b9e0a844451abab
0rcb0n3-lab <0rcb0n3@gmail.com>
2026-09-16 23:57:41 +0300

    add DialogFlow learning script.

---
+++ create_intent.py
@@ -0,0 +1,52 @@
+import json
+import os
+
+from environs import Env
+
+from google.cloud import dialogflow
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=[message_texts])
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message],
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print(f"Intent created: {response.display_name}")
+
+
+if __name__ == "__main__":
+    env = Env()
+    env.read_env()
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = env.str("GOOGLE_APPLICATION_CREDENTIALS")
+    project_id = env.str("DIALOGFLOW_PROJECT_ID")
+
+    with open("training_phrases.json", "r", encoding="utf-8") as file:
+        training_phrases = json.load(file)
+
+    for intent_name, phrases in training_phrases.items():
+        create_intent(
+            project_id=project_id,
+            display_name=intent_name,
+            training_phrases_parts=phrases["questions"],
+            message_texts=phrases["answer"],
+        )
