ecb7559b6bab1da46817d78fda934df028086a21
0rcb0n3-lab <0rcb0n3@gmail.com>
2026-09-16 23:08:43 +0300

    setup DialogFlow

--- bot.py
+++ bot.py
@@ -1,11 +1,28 @@
 import asyncio
 import logging
+import os
 
 from environs import Env
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+
+from google.cloud import dialogflow
+
+
+def get_dialogflow_response(project_id: str, session_id: str, text: str, language_code="ru") -> str:
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    return response.query_result.fulfillment_text
 
 
 async def main() -> None:
@@ -18,6 +35,9 @@ async def main() -> None:
     )
     logging.getLogger("aiogram").setLevel(logging.WARNING)
 
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = env.str("GOOGLE_APPLICATION_CREDENTIALS")
+    project_id = env.str("DIALOGFLOW_PROJECT_ID")
+
     bot = Bot(token=env.str("TG_BOT_TOKEN"))
     dp = Dispatcher()
 
@@ -27,8 +47,15 @@ async def main() -> None:
         await message.answer("Здравствуйте")
 
     @dp.message()
-    async def echo(message: Message) -> None:
-        await message.answer(message.text)
+    async def dialogflow_reply(message: Message) -> None:
+        reply = get_dialogflow_response(
+            project_id,
+            str(message.from_user.id),
+            message.text,
+            "ru-RU",
+        )
+        if reply:
+            await message.answer(reply)
 
     await dp.start_polling(bot)
 
