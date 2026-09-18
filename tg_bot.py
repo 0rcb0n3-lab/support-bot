dd6bc194da7f18976a9dff3a7cf0cde607d271a6
0rcb0n3-lab <0rcb0n3@gmail.com>
2026-09-18 19:14:00 +0300

    add DialogFlow to VK bot

--- tg_bot.py
+++ tg_bot.py
@@ -8,21 +8,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-from google.cloud import dialogflow
-
-
-def get_dialogflow_response(project_id: str, session_id: str, text: str, language_code="ru") -> str:
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
-
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
-    query_input = dialogflow.QueryInput(text=text_input)
-
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-
-    return response.query_result.fulfillment_text
+from dialogflow_handler import get_dialogflow_response
 
 
 async def main() -> None:
--- vk_bot.py
+++ vk_bot.py
@@ -1,12 +1,19 @@
+import os
+
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 from environs import Env
 
+from dialogflow_handler import get_dialogflow_response
+
 
 def main():
     env = Env()
     env.read_env()
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = env.str("GOOGLE_APPLICATION_CREDENTIALS")
+    project_id = env.str("DIALOGFLOW_PROJECT_ID")
 
     vk_session = vk_api.VkApi(token=env.str("VK_GROUP_TOKEN"))
     vk = vk_session.get_api()
@@ -20,7 +27,9 @@ def main():
             else:
                 print(f"Для меня от: {event.user_id}")
                 vk.messages.send(
-                    user_id=event.user_id, message=event.text, random_id=0
+                    user_id=event.user_id,
+                    message=get_dialogflow_response(project_id, str(event.user_id), event.text),
+                    random_id=0,
                 )
             print(f"Текст: {event.text}")
 
